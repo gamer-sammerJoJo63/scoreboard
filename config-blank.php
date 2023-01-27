0959fe406062ee69716ad49c139c3ef8f3c3145d
sammerJoJo63 <kikidumbblonde@gmail.com>
2023-01-26 21:50:02 -0500

    database config updated, new base files

---
+++ config-blank.php
@@ -0,0 +1,19 @@
+<?php
+
+//enter hosting info
+define('HOST', '');
+define('USERNAME', '');
+define('PASSWORD', '');
+define('DBNAME', '');
+
+define('TABLENAMEGAME', '');
+define('TABLENAMETEAMS', '');
+define('TABLENAMESCORES', '');
+
+
+// I use this for a menu if you have one, otherwise you
+// don't have to use this default. This is just something
+// I commonly have in my config files.
+define('BASEURL'. '');
+
+?>
---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('HOST', '162.253.224.12');
+define('USERNAME', 'samlcar2_samcarnegamenight');
+define('PASSWORD', '#scyrR4ua%nG72dzPchAvZ5NnE%nytq&YWznp96r%TT3paWP2uxx');
+define('DBNAME', 'samlcar2_scoreboard');
+
+define('TABLENAMEGAME', 'game_sessions');
+define('TABLENAMETEAMS', 'game_teams');
+define('TABLENAMESCORES', 'game_scores');
+
+define('BASEURL', 'https://games.samcarne.com/');
+?>
